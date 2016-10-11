6d8fb90ad7c344953365725c3a038d95d243faac
CédricM <c.metairie@gmail.com>
2016-10-11 12:52:16 +0200

    [*] fix products regexs

--- eoxiatheme/inc/blocs/functions.php
+++ eoxiatheme/inc/blocs/functions.php
@@ -183,17 +183,52 @@ function eox_get_dynamic( $atts ){
 
 
 function eox_get_last_products( $atts ){
-	echo '[wpshop_products limit="'.$atts['limit'].'" order="date" sorting="no" type="list"]';
-	$product_markup =  do_shortcode('[wpshop_products limit="'.$atts['limit'].'" order="date" sorting="no" type="list"]');
+	//echo '[wpshop_products limit="'.$atts['limit'].'" order="date" sorting="no" type="list"]';
+	$product_markup =  do_shortcode('[wpshop_products limit="'.$atts['limit'].'" order="date" sorting="No" type="list"]');
 	$product_markup = eox_rewrite_wps_product_tpl( $product_markup, $atts );
 	echo $product_markup;
 }
 
 function eox_rewrite_wps_product_tpl( $product_markup, $atts ){
-	//print_r( $atts );
-	$find = array('/(.+)<ul[^>]*>(.+)<\/ul>(.+)/i', '/<li[^>]*>(.*?)<\/li>/i', '/<span class="wps-caption"[^>]*>(.*?)<\/span>/i', '/<span class="wps-thumbnail"[^>]*>(.*?)<\/span>/i', '/<span class="wps-title"[^>]*>(.*?)<\/span>/i');
-	$replace = array('${2}', '<div class="'.$atts['bloc_class'].' -product-item">${1}</div>', '<span class="wps-caption" style="'.$atts['txt_style'] .'">${1}</span>', '<span class="wps-thumbnail" style="'.$atts['bg_style'] .'">${1}</span>', '<h3 class="bloc-title" style="'.$atts['txt_style'] .'">${1}</h3>' );
+	
+
+	//print_r( $product_markup );
+
+	// $find = array(
+	// 	'/(.+)<ul[^>]*>(.+)<\/ul>(.+)/i', 
+	// 	'/<li[^>]*>(.*?)<\/li>/i', 
+	// 	'/<span class="wps-caption"[^>]*>(.*?)<\/span>/i', 
+	// 	'/<span class="wps-thumbnail"[^>]*>(.*?)<\/span>/i', 
+	// 	'/<span class="wps-title"[^>]*>(.*?)<\/span>/i'
+	// );
+	// $replace = array(
+	// 	'${1}', 
+	// 	'<div class="'.$atts['bloc_class'].' -product-item">${1}</div>', 
+	// 	'<span class="wps-caption" style="'.$atts['txt_style'] .'">${1}</span>', 
+	// 	'<span class="wps-thumbnail" style="'.$atts['bg_style'] .'">${1}</span>', 
+	// 	'<h3 class="bloc-title" style="'.$atts['txt_style'] .'">${1}</h3>' 
+	// );
+
+	$find = array(
+		'/(.+)<ul(.*?)>(.*?)<\/ul>(.+)/s',
+		// '/(^<ul[^>]*>|<\/ul>$)/i',
+		'/<li(.*?)>(.*?)<\/li>/s',
+		'/<span class="wps-caption"(.*?)>(.*?)<\/span>/s',
+		'/<span class="wps-thumbnail"(.*?)>(.*?)<\/span>/s',
+		'/<span class="wps-title"(.*?)>(.*?)<\/span>/s'
+	);
+	$replace = array(
+		'$3',
+		//'',
+		'<div class="'.$atts['bloc_class'].' -product-item">$2</div>',
+		'<span class="wps-caption" style="'.$atts['txt_style'] .'">$2</span>',
+		'<span class="wps-thumbnail" style="'.$atts['bg_style'] .'">$2</span>',
+		'<h3 class="bloc-title" style="'.$atts['txt_style'] .'">$2</h3>'
+	);
+
+	//print_r( $product_markup );
 	$product_markup = preg_replace( $find, $replace, $product_markup );
+	//print_r( $product_markup );
 	$product_markup =  preg_replace_callback('/<p class="bloc-excerpt"[^>]*>(.*?)<\/p>/i' , function( $match ) use ( $atts ) { return ( '<p class="bloc-excerpt">'.wp_trim_words($match[1], $atts['excerptlength'] ) ).'</p>'; } , $product_markup);
 	$product_markup = ( $atts['mode'] == '-m-background' ) ? preg_replace('/<div class="bloc-item-padder"[^>]*>(.*?)<\/div>/i', '<div class="bloc-item-padder" style="'.$atts['bg_style'] .'">${1}</div>' , $product_markup) : $product_markup;
 	return $product_markup;
