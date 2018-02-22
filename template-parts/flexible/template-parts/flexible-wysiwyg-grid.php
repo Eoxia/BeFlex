68c444ebc0c8118a9bd33dbcfd7adb3836d2729e
Amandine Eoxia <amandine@eoxia.com>
2018-02-22 10:06:09 +0100

    2.2.0: Fix sidebar & flexible wysiwyg grid

--- template-parts/flexible/template-parts/flexible-wysiwyg-grid.php
+++ template-parts/flexible/template-parts/flexible-wysiwyg-grid.php
@@ -8,23 +8,29 @@
  * @package beflex
  */
 
-if ( have_rows( 'liste_wysiwyg' ) ) :
-	while ( have_rows( 'liste_wysiwyg' ) ) : the_row();
+?>
+<div class="site-layout content">
+	<?php
+	if ( have_rows( 'liste_wysiwyg' ) ) :
+		while ( have_rows( 'liste_wysiwyg' ) ) : the_row();
 
-		$title = get_sub_field( 'titre_de_la_section' );
-		$width = get_sub_field( 'largeur' ) ? get_sub_field( 'largeur' ) : '100'; ?>
+			$title = get_sub_field( 'titre_de_la_section' );
+			$width = get_sub_field( 'largeur' ) ? get_sub_field( 'largeur' ) : '100'; ?>
 
-		<div class="content" style="width: <?php echo esc_html( $width ); ?>%;">
-			<?php if ( ! empty( $title ) ) : ?>
-				<header class="entry-header">
-					<h2 class="section-title"><?php echo esc_html( $title ); ?></h2>
-				</header><!-- .entry-header -->
-			<?php endif; ?>
+			<div class="grid-content" style="width: <?php echo esc_html( $width ); ?>%;">
+				<?php if ( ! empty( $title ) ) : ?>
+					<header class="entry-header">
+						<h2 class="section-title"><?php echo esc_html( $title ); ?></h2>
+					</header><!-- .entry-header -->
+				<?php endif; ?>
 
-			<div class="bloc-container">
-				<?php the_sub_field( 'relation_wysiwyg' ); ?>
-			</div>
-		</div> <?php
+				<div class="bloc-container">
+					<?php the_sub_field( 'relation_wysiwyg' ); ?>
+				</div>
+			</div> <?php
 
-	endwhile;
-endif;
+		endwhile;
+	endif;
+	?>
+</div>
+<?php
